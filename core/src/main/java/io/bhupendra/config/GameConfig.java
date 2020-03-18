810ad5454959a25f321e73ea8b6db395c7f7438e
bhupendra <bhupendrapt95@gmail.com>
2020-03-18 23:03:11 +0530

    game.properties file added in core module

--- core/src/main/java/io/bhupendra/config/GameConfig.java
+++ core/src/main/java/io/bhupendra/config/GameConfig.java
@@ -2,16 +2,21 @@ package io.bhupendra.config;
 
 import io.bhupendra.GuessCount;
 import io.bhupendra.MaxNumber;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource("classpath:config/game.properties")
 public class GameConfig {
 
     //== fields ===
-    private int maxNumber = 50 ;
+    @Value("${game.maxNumber: 50}")
+    private int maxNumber;
 
-    private int guessCount = 8;
+    @Value("${game.guessCount: 8}")
+    private int guessCount ;
 
     // == bean methods ==
     @Bean
